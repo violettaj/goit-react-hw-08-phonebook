b48d5e7b14a4601d527baf95f0d5799077298508
violettaj <107106832+violettaj@users.noreply.github.com>
2023-03-28 10:56:27 +0000

    Deploying to gh-pages from @ violettaj/goit-react-hw-08-phonebook@21603eaf9787761b5a405c2a991a6d7ae9619172 🚀

---
+++ static/js/151.29bca8be.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[151],{3151:function(e,t,r){r.r(t),r.d(t,{default:function(){return a}});r(2791);var c="Home_title__qAdt+",n=r(184),a=function(){return(0,n.jsx)("div",{children:(0,n.jsx)("h1",{className:c,children:"Welcome to PHONEBOOK"})})}}}]);
+//# sourceMappingURL=151.29bca8be.chunk.js.map
